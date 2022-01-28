ee06529e0b64b535c5491a1cf9da1b1d30bad67d
thelastdB <finley.ryan@gmail.com>
2022-01-28 05:38:52 -0800

    Adding activity solution.

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 import sys
 import json
+from urllib import response
 
 import requests
 
-# Use Like python githubber.py JASchilz
-# (or another user name)
-
 if __name__ == "__main__":
-    username = sys.argv[1]
+    username = input("Enter a Github username: ")
 
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
+    response = requests.get(f"https://api.github.com/users/{username}/events")
 
-    print("COMPLETE THE TODOs")
-    
+    events = json.loads(response.content)
 
-
+    print(events[0]['created_at'])
